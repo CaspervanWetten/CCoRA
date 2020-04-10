9d62c6a62aedc7be4464cd0cb02d219770edcc5c
Lucas Steehouwer <lucas_steehouwer@hotmail.com>
2020-04-10 02:30:03 +0200

    Middleware for CORS
    With this middleware, even if you do not have access to the
    configuration of your server software of choice, you can still enable
    CORS. Configuration settings will have to be made so that the user can
    easily configure this setting.

---
+++ server/src/MiddleWare/CORS.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Cora\MiddleWare;
+
+use Psr\Http\Message\RequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+class CORS extends MiddleWare {
+    protected $allow;
+
+    public function __construct(string $allow) {
+        $this->allow = $allow;
+    }
+
+    public function __invoke(
+        Request $request,
+        Response $response,
+        callable $next) {
+        $response = $next($request, $response);
+        $newResponse = $response->withHeader(
+            "Access-Control-Allow-Origin",
+            $this->allow);
+        return $newResponse;
+    }
+}
